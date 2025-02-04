b5fbf5f13adbc8bb47e230a5ccab86d5dea47025
Mateyw <mateusz.mazik@adidas.com>
2025-02-04 22:29:43 +0100

    Added styles with Tailwindcss

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -6,13 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
 // Define a simple route
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index.ejs');
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
