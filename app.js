35297c13cf53ab04aa1e608d776c17030b4f57d1
Mateyw <mateusz.mazik@adidas.com>
2025-02-05 08:30:03 +0100

    Added ejs title property

--- app.js
+++ app.js
@@ -12,22 +12,22 @@ app.use(express.static('public'));
 
 // Define a simple route
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', { title: 'Home' });
 });
 app.get('/menus', (req, res) => {
-    res.render('menus.ejs');
+    res.render('menus.ejs', { title: 'Menus' });
 });
 app.get('/products', (req, res) => {
-    res.render('products.ejs');
+    res.render('products.ejs', { title: 'Products' });
 });
 app.get('/contest', (req, res) => {
-    res.render('contest.ejs');
+    res.render('contest.ejs', { title: 'Contest' });
 });
 app.get('/login', (req, res) => {
-    res.render('login.ejs');
+    res.render('login.ejs', { title: 'Login' });
 });
 app.get('/signup', (req, res) => {
-    res.render('singup.ejs');
+    res.render('singup.ejs', { title: 'Sign Up' });
 });
 
 
