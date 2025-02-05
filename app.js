1f60558afc2ef11738a41c10edc66d7b6c4d465b
Mateyw <mateusz.mazik@adidas.com>
2025-02-05 08:26:08 +0100

    Added more styles

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
+app.get('/menus', (req, res) => {
+    res.render('menus.ejs');
+});
+app.get('/products', (req, res) => {
+    res.render('products.ejs');
+});
+app.get('/contest', (req, res) => {
+    res.render('contest.ejs');
+});
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+app.get('/signup', (req, res) => {
+    res.render('singup.ejs');
+});
+
 
 // Start the server
 app.listen(PORT, () => {
---
+++ index.js
@@ -0,0 +1,5 @@
+const btnMenus = document.querySelector(".btn-menus")
+
+btnMenus.addEventListener("click", () => {
+    btnMenus.style.fontWeight = "bold";
+});
